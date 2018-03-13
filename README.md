188221947964a0def7474e7e19110100b05ed4df
Bill Anderson <therealbill@me.com>
2018-03-13 14:30:48 -0500

    adding homedir generation for configs which do not properly export the homedir. \n\n Also added profile selection to options/env.

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Ok, so I've been using this on my Raspberry Pi network for a while now, figure
 I'd put it here for public consumption. This is intended to be run as a Postfix
 delivery command. For example, if you have a address "upload@foo.com", then if
 you configure that alias to be `upload@foo.com |"path/to/pf2s3 -b BUCKET-NAME
--p PATH-IN-BUCKET " and have the needed aws credentials set  `.aws/creds` file
-for the user it will run as, then Postfix will send the message over standard in
-and the executable will upload it.
+-p PATH-IN-BUCKET " and have the needed aws credentials set via the
+`$HOME/.aws/credentials` file for the user it will run as, then Postfix will
+send the message over standard in and the executable will upload it.
 
 For the name of the object in s3 it will extract the Message-Id header it finds
 in the email. It will also extract the sender and recipient address and set
--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"net/mail"
 	"os"
+	"os/user"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/urfave/cli"
@@ -40,12 +43,20 @@ func main() {
 			EnvVar: "AWS_REGION",
 			Value:  "us-east-1",
 		},
+		cli.StringFlag{
+			Name:   "profile,u",
+			Usage:  "The AWS profile to get creds from the credentials file for",
+			EnvVar: "AWS_PROFILE",
+			Value:  "default",
+		},
 	}
 	app.Action = sendToS3
 	app.Run(os.Args)
 }
 
 func sendToS3(c *cli.Context) error {
+	user, _ := user.Current()
+	fmt.Printf("%+v\n", user)
 	bucket := c.String("bucket")
 	base_path := c.String("path")
 	region := c.String("region")
@@ -69,15 +80,17 @@ func sendToS3(c *cli.Context) error {
 	}
 	tags := fmt.Sprintf("sender=%s&recipient=%s", fromhdr.Address, tohdr.Address)
 
-	s, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	os.Setenv("HOME", user.HomeDir)
+	s, err := session.NewSession(&aws.Config{Region: aws.String(region), Credentials: credentials.NewSharedCredentials("", "default")})
 	if err != nil {
 		log.Fatal(err)
 	}
+	mid := strings.Trim(m.Header.Get("Message-Id"), "<>")
 	mreader.Seek(0, 0)
 
 	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
 		Bucket:               aws.String(bucket),
-		Key:                  aws.String(base_path + "/" + m.Header.Get("Message-Id")),
+		Key:                  aws.String(base_path + "/" + mid),
 		Body:                 mreader,
 		ServerSideEncryption: aws.String("AES256"),
 		Tagging:              aws.String(tags),
